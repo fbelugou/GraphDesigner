b93765d857fac1f0ae05a1dbf7885557c109490d
fbelugou <fbelugou@gmail.com>
2015-04-03 13:58:58 +0200

    Add Library

---
+++ www/js/lib/bdd.js
@@ -0,0 +1,24 @@
+//////////////////////////////////////////////
+//										 /////
+//	Base de donnee algorithme description ////
+//	titre,description,imagePath,complexité ///
+//////////////////////////////////////////////
+
+
+//Parcours
+var parcoursProfondeur_D = {titre:"Parcours Profondeur", desc:"ParcoursProfondeur Description", image: "", complexite:"", cmd:"parcoursProfondeur", args:{"length":1, "0":1 } };
+var parcoursLargeur_D = {titre:"Parcours Largeur", desc:"", image: "", complexite:"", cmd:"parcoursLargeur", args:{"length":1, "0":1 } };
+
+//Arbre
+var kruskal_D = {titre:"Kruskal", desc:"", image: "images/kruskal.jpg", complexite:"",cmd:"kruskal", args:{"length":1, "0":1 } };
+var prim_D = {titre:"Prim", desc:"", image: "images/Robert-C-Prim.jpg", complexite:"",cmd:"prim", args:{"length":1, "0":1 } };
+
+//Chemins
+var circuitEulerien_D = {titre:"Circuit Eulerien", desc:"", image: "images/euler.jpg", complexite:"", cmd:"circuitEulerien", args:{"length":1, "0":1 } };
+var cheminEulerien_D = {titre:"Chemin Eulerin", desc:"", image: "images/euler.jpg", complexite:"", cmd:"cheminEulerien", args:{"length":1, "0":1 } };
+var dijkstra_D = {titre:"Disjkstra", desc:"", image: "images/dijkstra.jpg", complexite:"", cmd:"dijkstra", args:{"length":1, "0":1 } };
+var bellman_D = {titre:"Bellman", desc:"", image: "images/bellman.jpg", complexite:"", cmd:"bellman", args:{"length":1, "0":1 } };
+
+//Coloration
+var naif_D = {titre:"Naif", desc:"", image: "", complexite: "", cmd:"naif", args:{"length":1, "0":1 } };
+var glouton_D = {titre:"Glouton", desc:"", image: "", complexite: "", cmd:"glouton", args:{"length":1, "0":1 } }; 
